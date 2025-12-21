06f579c25cfb0e2e15dcd6e6efda3ecda7d4f041
LTF92 <63012547+LTF92@users.noreply.github.com>
2025-12-21 22:27:18 +0300

    Clean up: remove unnecessary files (VDB scripts, old docs)

--- load_arabic_poems.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-
-# تحميل مجموعة بيانات القصائد العربية من Hugging Face
-ds = load_dataset("alwalid54321/Arabic_Poems")
-
-# عرض معلومات عن مجموعة البيانات
-print(ds)
-
